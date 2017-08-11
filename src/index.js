f217ac8276e880ae0ea71be163d64b416487c8df
James J Dooley <jimdooley@Jamess-MacBook-Pro.local>
2017-08-11 14:29:12 -0400

    current prg

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-document.getElementById("map").innerText = "Hello from JS";
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1Ijoiampkb29sZXkiLCJhIjoiY2o2ODQ1dXNrMGQzejJ3bG1lb3ozNmp1OCJ9.ltqfS9u3cbP9eLvH8_O21Q";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack coordinates
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available
+});
